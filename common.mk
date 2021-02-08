3cdb4008790ed1646d13298265ed41c5a6890a22
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:36:57 +0100

    sm8250-common: Setup namespace
    Change-Id: I97172190de6bcf572021b5a252a1dc587e1a3fc4

--- common.mk
+++ common.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit proprietary blobs
 $(call inherit-product, vendor/samsung/sm8250-common/sm8250-common-vendor.mk)
