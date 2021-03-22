9b3380efafbe244369688cc8afaa1a6992c2e7b2
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-03-22 15:58:08 +0100

    sm8250-common: Import powerhint.json from android-11.0.0_r32
    Change-Id: I552870c130ceaac4d20322acf6d1da22ad5017ec

--- common.mk
+++ common.mk
@@ -290,6 +290,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power@1.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # PowerShare
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.samsung
