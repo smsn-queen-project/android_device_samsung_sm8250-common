6101637b5bdbfb2be1539c44a8ed93c1b9a3be9a
Bruno Martins <bgcngm@gmail.com>
2024-05-18 13:21:42 +0100

    sm8250-common: Use common libqti-perfd-client
    Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd

--- common.mk
+++ common.mk
@@ -225,6 +225,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
@@ -355,6 +359,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/samsung
 
 # Tether
