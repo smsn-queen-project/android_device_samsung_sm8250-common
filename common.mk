c97f42847766be844f9d05c227d29d049d3393b4
Aryan <basam.aryan@gmail.com>
2024-06-03 20:34:31 +0700

    sm8250-common: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    For now this is implicitly built by lineage specific
    libprotobuf-cpp-lite-3.9.1-vendorcompat lib, but in the future this
    might not be the case.
    
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da

--- common.mk
+++ common.mk
@@ -154,6 +154,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Protobuf
 PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Public libraries
