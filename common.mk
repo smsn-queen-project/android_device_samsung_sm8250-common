cda00c3bcd4f7c4a5b6c249e13409c86bb896534
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 20:40:21 +0300

    sm8250-common: Import media configs
    From G780GXXSCEXI1
    
    Cleaned up from SEC software.c2 codecs.
    
    Change-Id: I44233c4e8fc20b62f3ec30a5a828ca99fb6d5b18

--- common.mk
+++ common.mk
@@ -176,6 +176,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.samsung-qcom-sm8250
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_kona.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_kona.xml \
+    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
