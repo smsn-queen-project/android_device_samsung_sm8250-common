2935ee47f4f85ab78f24e3ecad4e9d9254a18529
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 19:59:17 +0300

    sm8250-common: Set up overlays
    Change-Id: I87672d4c548eb57aac0a8251d8073e438812f84d

--- common.mk
+++ common.mk
@@ -189,6 +189,13 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
