2d404ca8ff05f67b82c93ca6c15fd0d82bf0c5d1
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 15:15:05 +0300

    sm8250-common: Import public.libraries.txt
    Change-Id: If851d792aec073eb030c6a77482c62866fe77bc8

--- common.mk
+++ common.mk
@@ -142,6 +142,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libcurl.vendor \
