78debf0e9c1cc77d9266ea9193f13decc9e7dedd
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 21:29:02 +0300

    sm8250-common: Build libavservices_minijail
    Change-Id: Id117ebdfe6cd0ce0e8d5063bace648dd2a2b0148

--- common.mk
+++ common.mk
@@ -185,6 +185,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+# Minijail
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
