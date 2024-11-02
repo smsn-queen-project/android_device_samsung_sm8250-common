597556280aff6faefc147ca9a72c8dae97b22cc6
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 05:32:17 +0300

    sm8250-common: Set kernel flags
    Change-Id: Ib6d9ecdab25d875025536cf58e9f663dbc812270

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,31 @@
 
 COMMON_PATH := device/samsung/sm8250-common
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_BASE         := 0x00000000
+BOARD_KERNEL_PAGESIZE     := 4096
+BOARD_MKBOOTIMG_ARGS      += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+BOARD_KERNEL_CMDLINE := console=null \
+                        androidboot.hardware=qcom \
+                        androidboot.memcg=1 \
+                        lpm_levels.sleep_disabled=1 \
+                        video=vfb:640x400,bpp=32,memsize=3072000 \
+                        msm_rtb.filter=0x237 \
+                        service_locator.enable=1 \
+                        androidboot.usbcontroller=a600000.dwc3 \
+                        swiotlb=2048 \
+                        printk.devkmsg=on \
+                        firmware_class.path=/vendor/firmware_mnt/image \
+                        loop.max_part=7
+
+BOARD_KERNEL_IMAGE_NAME      := Image
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_SEPARATED_DTBO  := true
+TARGET_KERNEL_SOURCE         := kernel/samsung/sm8250
+TARGET_KERNEL_CONFIG         := vendor/kona-sec-perf_defconfig \
+                                vendor/debugfs.config
+
 # Inherit the proprietary files
 include vendor/samsung/sm8250-common/BoardConfigVendor.mk
