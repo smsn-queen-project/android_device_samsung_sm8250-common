a76b31b8d58f99573889ee57fd077ab4c4adbbfb
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 10:46:22 +0300

    sm8250-common: Setup bluetooth
    Change-Id: Ibe4448641aea7947514327f21d2f171d7c2183de

--- common.mk
+++ common.mk
@@ -32,6 +32,15 @@ PRODUCT_PACKAGES += \
 
 TARGET_EXCLUDES_AUDIOFX := true
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    audio.bluetooth.default \
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth@1.1.vendor
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
