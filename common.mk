ec3dc05f5e245aceba67ed41f04bce629728d9b8
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 15:10:53 +0300

    sm8250-common: Build FlipFlap
    Change-Id: Ie0dbaba77ad219d672be96a17cf5908b4a3ad545

--- common.mk
+++ common.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.samsung
 
+# FlipFlap
+PRODUCT_PACKAGES += \
+    FlipFlap
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
