2b9ec4cc9753a46cc900b8d4e42e91ce2eb5439f
ata-kaner <ata.kaner03@gmail.com>
2024-11-02 10:45:00 +0300

    sm8250-common: Setup audio
    Change-Id: Ic6c7d5d463dc43ef767a4d78fc20b77193e54e42

--- common.mk
+++ common.mk
@@ -4,6 +4,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
+    android.hardware.audio@7.0-impl.samsung-sm8250 \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    libaudiopreprocessing \
+    libaudioroute.vendor \
+    libbundlewrapper \
+    libdownmix \
+    libdynproc \
+    libeffectproxy \
+    libldnhncr \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libreverbwrapper \
+    libtinycompress \
+    libvisualizer \
+    libvolumelistener \
+    SamsungDAP
+
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
