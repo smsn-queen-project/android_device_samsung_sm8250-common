7ce36acc9b8a07a9c8c8bbdb24e93bbb2225fd0c
Kelvin Zhang <zhangkelvin@google.com>
2024-11-23 18:42:43 +0100

    sm8250-common: Allow to access bootable/deprecated-ota targets
    Sm8250-common is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to sm8250-common namespace list.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- common.mk
+++ common.mk
@@ -311,6 +311,7 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client \
